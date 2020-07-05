eb7f90058efbe616b0265e8e09d9c3ab4479ec04
jkvim <jkvim@outlook.com>
2020-07-05 15:40:17 +0800

    feature: update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,5 +4,13 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>About</h1>
+    <div className="about__body">
+      <h2 className="about__body-title">个人作品</h2>
+      <ul>
+        <li>
+          <a href="https://one-habit-landing-page.vercel.app/">One Habit</a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 )
